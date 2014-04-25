aefef50ab773e62f0ba4824d0a69b7898e8b0bbd
George Straehle <gstraehle@gmail.com>
2014-04-24 23:32:24 -0400

    All simple getter/setter attributes working

--- features/create_cars.rb
+++ features/create_cars.rb
@@ -1,4 +1,6 @@
 require_relative '../lib/car'
 
- uglypontiac = Car.new("pontiac","aztec", 2006)
- puts "The car is a #{uglypontiac.make} #{uglypontiac.model} from #{uglypontiac.model_year}."
+ uglypontiac = Car.new("pontiac","aztec", 2006, "red", "auto", 20_000, 43_500, 850, "good")
+ puts "The car is a #{uglypontiac.color} #{uglypontiac.make} #{uglypontiac.model} from #{uglypontiac.model_year}."
+ puts "This #{uglypontiac.transmission} trans car was worth #{uglypontiac.msrp}"
+ puts "Now has #{uglypontiac.mileage} miles, #{uglypontiac.repairs_needed} worth of needed repairs and is in #{uglypontiac.condition} condition"
--- lib/car.rb
+++ lib/car.rb
@@ -15,20 +15,20 @@ class Car
 #     @car_id += 1
 #   end
    #getter method for items staying constant
-   attr_reader :make, :model, :model_year #, :msrp, :color, :transmission, :car_id,
-#   #setter method for items that may change
-#   attr_accessor :mileage, :damage, :repairs_needed
+   attr_reader :make, :model, :model_year, :msrp, :color, :transmission#, :car_id,
+   #setter method for items that may change
+   attr_accessor :mileage, :damage, :repairs_needed, :condition
 
-   def initialize(make, model, year)# color, trans, msrp, miles, damage, condition)
+   def initialize(make, model, year, color, trans, msrp, miles, damage_val, condition)
      @make = make
      @model = model
      @model_year = year
-#     @color = color
-#     @transmission = trans
-#     @msrp = msrp
-#     @mileage = miles
-#     @damage = damage
-#     @repairs_needed = condition
+     @color = color
+     @transmission = trans
+     @msrp = msrp
+     @mileage = miles
+     @repairs_needed = damage_val
+     @condition = condition
 
 #     @car_id = Car.gen_car_id
    end
