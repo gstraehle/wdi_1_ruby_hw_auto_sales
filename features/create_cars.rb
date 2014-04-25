917a2ea671c63e9ab0a9b4306c5a7799b2c25f35
George Straehle <gstraehle@gmail.com>
2014-04-25 00:05:36 -0400

    car ID asignment is working

--- features/create_cars.rb
+++ features/create_cars.rb
@@ -6,4 +6,6 @@ require_relative '../lib/car'
  puts "This #{uglypontiac.transmission} trans car was worth #{uglypontiac.msrp}"
  puts "Now has #{uglypontiac.mileage} miles, #{uglypontiac.repairs_needed} worth of needed repairs and is in #{uglypontiac.condition} condition"
  puts "The car is #{uglypontiac.car_age} years old."
+ puts "ID number #{uglypontiac.car_id}."
+# uglydodge = Car.new("dodge","neon", 2003, "pink", "man", 200_000, 430_500, 150, "poor")
 
--- lib/car.rb
+++ lib/car.rb
@@ -5,20 +5,21 @@ class Car
 #   #depreciation per year as a rate, roughly a constant
 #   DEPRECIATION = .8
 #   # class variable
-#   @car_id = 10_000_000_000
+   @car_id = 10_000_000_000
 
-#   def self.car_id
-#     @car_id
-#   end
+   def self.car_id
+     @car_id
+   end
 
-#   def self.gen_car_id
-#     @car_id += 1
-#   end
+   def self.gen_car_id
+     @car_id += 1
+   end
    #getter method for items staying constant
-   attr_reader :make, :model, :model_year, :msrp, :color, :transmission#, :car_id,
+   attr_reader :make, :model, :model_year, :msrp, :color, :transmission, :car_id
    #setter method for items that may change
    attr_accessor :mileage, :damage, :repairs_needed, :condition
 
+    # initialized statements do not match word-for-word with inst. var names
    def initialize(make, model, year, color, trans, msrp, miles, damage_val, condition)
      @make = make
      @model = model
@@ -30,7 +31,7 @@ class Car
      @repairs_needed = damage_val
      @condition = condition
 
-#     @car_id = Car.gen_car_id
+     @car_id = Car.gen_car_id
    end
 
    def car_age
